6136fb911804d2cabf069a2881f319bd825aecb4
godly-devotion <jpark@duck.com>
2022-11-15 20:27:31 -0500

    libreddit: set cookies

--- src/Redirect Reddit to Libreddit.user.js
+++ src/Redirect Reddit to Libreddit.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         Redirect Reddit to Libreddit
-// @version      1
+// @version      2
 // @encoding     utf-8
 // @match        *://*.reddit.com/*
+// @match        *://libreddit.nl/*
 // @author       godly-devotion
 // @compatible   chrome
 // @compatible   firefox
@@ -14,7 +15,21 @@
 // @run-at       document-start
 // ==/UserScript==
 
-const pathname = window.location.pathname;
-if (pathname) {
-  window.location.replace("https://libreddit.nl" + pathname);
+if (window.location.hostname === "www.reddit.com") {
+  const pathname = window.location.pathname;
+  if (pathname) {
+    window.location.replace("https://libreddit.nl" + pathname);
+  }
+  return;
+}
+
+if (window.location.hostname === "libreddit.nl") {
+  document.cookie = "front_page=default; Secure";
+  document.cookie = "post_sort=hot; Secure";
+  document.cookie = "comment_sort=top; Secure";
+  document.cookie = "hide_hls_notification=on; Secure";
+  document.cookie = "layout=card; Secure";
+  document.cookie = "theme=system; Secure";
+  document.cookie = "use_hls=on; Secure";
+  document.cookie = "wide=off; Secure";
 }
