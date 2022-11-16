49499866fab72c10f92343df2bc540f7b02c23c3
godly-devotion <jpark@duck.com>
2022-11-15 20:42:50 -0500

    nitter: set cookies

--- src/Redirect Twitter to Nitter.user.js
+++ src/Redirect Twitter to Nitter.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         Redirect Twitter to Nitter
-// @version      4
+// @version      6
 // @encoding     utf-8
 // @match        *://*.twitter.com/*
+// @match        *://nitter.nl/*
 // @author       godly-devotion
 // @compatible   chrome
 // @compatible   firefox
@@ -14,7 +15,23 @@
 // @run-at       document-start
 // ==/UserScript==
 
-const pathname = window.location.pathname;
-if (pathname) {
-  window.location.replace("https://nitter.nl" + pathname);
+if (window.location.hostname === "twitter.com") {
+  const pathname = window.location.pathname;
+  if (pathname) {
+    window.location.replace("https://nitter.nl" + pathname);
+  }
+  return;
+}
+
+if (window.location.hostname === "nitter.nl") {
+  if (localStorage.getItem("reloadForCookies")) {
+    return;
+  }
+  document.cookie = "hlsPlayback=on; Secure";
+  document.cookie = "infiniteScroll=on; Secure";
+  document.cookie = "replaceReddit=libreddit.nl; Secure";
+  document.cookie = "replaceTwitter=nitter.nl; Secure";
+  document.cookie = "replaceYouTube=; Secure";
+  localStorage.setItem("reloadForCookies", "true");
+  window.location.reload();
 }
