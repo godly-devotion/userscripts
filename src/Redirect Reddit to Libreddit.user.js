a9d8cf12efe192fbd5853813beb01a2bcce33ea1
godly-devotion <jpark@duck.com>
2022-11-18 09:04:43 -0500

    libreddit: do not show nsfw

--- src/Redirect Reddit to Libreddit.user.js
+++ src/Redirect Reddit to Libreddit.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Redirect Reddit to Libreddit
-// @version      3
+// @version      4
 // @encoding     utf-8
 // @match        *://*.reddit.com/*
 // @match        *://libreddit.nl/*
@@ -27,13 +27,14 @@ if (window.location.hostname === "libreddit.nl") {
   if (document.cookie) {
     return;
   }
+  document.cookie = "theme=system; path=/";
   document.cookie = "front_page=default; path=/";
+  document.cookie = "layout=card; path=/";
+  document.cookie = "wide=off; path=/";
   document.cookie = "post_sort=hot; path=/";
   document.cookie = "comment_sort=top; path=/";
-  document.cookie = "hide_hls_notification=on; path=/";
-  document.cookie = "layout=card; path=/";
-  document.cookie = "theme=system; path=/";
+  document.cookie = "show_nsfw=off; path=/";
   document.cookie = "use_hls=on; path=/";
-  document.cookie = "wide=off; path=/";
+  document.cookie = "hide_hls_notification=on; path=/";
   window.location.reload();
 }
