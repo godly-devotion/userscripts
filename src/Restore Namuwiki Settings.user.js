6cc6a81259f75578855a5624d848c9e030d34cf4
godly-devotion <jpark@duck.com>
2024-07-15 23:45:33 -0400

    feat: redirect namuwiki to Korean

--- src/Restore Namuwiki Settings.user.js
+++ src/Restore Namuwiki Settings.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         Restore Namuwiki Settings
-// @version      1
+// @version      2
 // @encoding     utf-8
 // @match        *://namu.wiki/*
+// @match        *://en.namu.wiki/*
 // @author       godly-devotion
 // @compatible   chrome
 // @compatible   firefox
@@ -14,9 +15,21 @@
 // @run-at       document-start
 // ==/UserScript==
 
-const settings = localStorage.getItem('theseed_settings');
+// redirect to Korean site
+if (window.location.hostname !== "namu.wiki") {
+  const pathname = window.location.pathname;
+  if (pathname) {
+    window.location.replace("https://namu.wiki/trdr?redirect=" + pathname);
+  }
+  return;
+}
+
+const settings = localStorage.getItem("theseed_settings");
 if (settings) {
   return;
 }
-localStorage.setItem('theseed_settings', '{"wiki.footnote_type":"popover","wiki.dark_mode":true,"wiki.forced_font_size":"1.3rem","wiki.disable_image_lazy":true,"discuss.strike":"hide","discuss.bold":"hide","senkawa.hide_sidebar":true,"senkawa.left_sidebar":false,"senkawa.hide_namunews_card":true}');
+localStorage.setItem(
+  "theseed_settings",
+  '{"wiki.footnote_type":"popover","wiki.dark_mode":true,"wiki.forced_font_size":"1.3rem","wiki.disable_image_lazy":true,"discuss.strike":"hide","discuss.bold":"hide","senkawa.hide_sidebar":true,"senkawa.left_sidebar":false,"senkawa.hide_namunews_card":true}'
+);
 window.location.reload();
